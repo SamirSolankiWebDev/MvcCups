c2afa8d58f62a51772bab725192b55caf9128bfe
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-16 23:50:17 -0400

    Commit Test

---
+++ Data/MvcCupsContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcCups.Models;
+
+namespace MvcCups.Data
+{
+    public class MvcCupsContext : DbContext
+    {
+        public MvcCupsContext(DbContextOptions<MvcCupsContext> options)
+              : base(options)
+        {
+        }
+
+        public DbSet<Cup> Cup { get; set; }
+    }
+}
