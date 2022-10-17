e5cd5b0a9a5e3c29a9f89d4305c95fb281de9333
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-16 20:50:34 -0400

    Done changes in New HelloWorldController.cs File

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCups.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        /* public IActionResult Index()
+         {
+             return View();
+         }*/
+
+        // 
+        // GET: /HelloWorld/
+
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
