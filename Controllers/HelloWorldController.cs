e3a4cd07c4f5bdf33812ba1cb2c5381683d709d3
Samir Ashok Solanki <W0785689@myscc.ca>
2022-10-16 21:15:18 -0400

    Commit Added New File

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -9,18 +9,18 @@ namespace MvcCups.Controllers
 {
     public class HelloWorldController : Controller
     {
-        /* public IActionResult Index()
-         {
-             return View();
-         }*/
+        public IActionResult Index()
+        {
+            return View();
+        }
 
         // 
         // GET: /HelloWorld/
 
-        public string Index()
+       /* public string Index()
         {
             return "This is my default action...";
-        }
+        }*/
 
         // 
         // GET: /HelloWorld/Welcome/ 
